6dea66b75aaeec15c8ced53c7d3c91cd0c473da5
danielgy <471379891@qq.com>
2018-06-30 15:49:56 +0800

    Update data_pre.py

--- data_pre.py
+++ data_pre.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 31 18:52:57 2018
+@author: GY
+"""
 import numpy as np
 import pandas as pd
+
 
 class DataSet(object):
     def __init__(self,
@@ -8,10 +14,9 @@ class DataSet(object):
                  reshape=False):
         self._num_examples = images.shape[0]
         self._images = images
-#TODO: one-hot encode
-        df=pd.get_dummies(labels)
-        labels=(np.array(df)).astype('float')
-#              
+        # TODO: one-hot encode
+        df = pd.get_dummies(labels)
+        labels = (np.array(df)).astype('float')            
         self._labels = labels
         self._epochs_completed = 0
         self._index_in_epoch = 0
@@ -50,19 +55,19 @@ class DataSet(object):
         end = self._index_in_epoch
         return self._images[start:end], self._labels[start:end]
 
+
 def input_data():
     import h5py
     import pandas as pd
 
-    h5f = h5py.File('F:\\PycharmProjects\\github\\CNN_series\\GAN\\Datas\\train.h5', 'r')
+    h5f = h5py.File('./train.h5', 'r')
     data = h5f['train_data'][:]
     temp = np.zeros([data.shape[0], 24 * 24 - data.shape[1]])
     data = np.hstack((data, temp))
     label = h5f['train_label'][:]
-   
-    label=pd.get_dummies(label)
-    label = np.array(label)
 
+    label = pd.get_dummies(label)
+    label = np.array(label)
 
     data = DataSet(np.array(data), np.array(label.reshape([-1, 2])))
     return data
